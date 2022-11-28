baa56e93d6fa7be98594c8b56cd21bf5c4c97f86
Ideal <ideal.buzoku.21@gmail.com>
2022-11-28 03:26:01 +0000

    Finish calculator.
    Write equation evaluator and js for buttons.

---
+++ main.js
@@ -0,0 +1,23 @@
+
+document.querySelector("#enter").addEventListener("click", () => {
+    const input = document.querySelector("input").value;
+    const output = document.querySelector("#output");
+
+    output.childNodes.forEach(c => c.nodeType === Node.TEXT_NODE && c.remove());
+    let output_text = document.createTextNode(`${Function("return " + input)()}`)
+    output.appendChild(output_text);
+})
+/*
+document.querySelectorAll(".row > button").forEach(button => {
+    content.log(button);
+    button.addEventListener("click", (button) => {
+        document.querySelector("input").innerHTML += button.innerHTML;
+    })
+});
+*/
+const buttons = document.querySelectorAll(".row button");
+for (let i = 0; i < buttons.length; i++)  {
+    buttons[i].addEventListener("click", function() {
+        document.querySelector("input").value += buttons[i].textContent;
+    })  
+}
